4d334eef11149277397ec273290d8ec994047ad0
farhad1372 <farhadfallahpour.eleceng@gmail.com>
2023-01-04 21:24:39 +0330

    add resources

--- src/pages/resources/index.vue
+++ src/pages/resources/index.vue
@@ -1,7 +1,13 @@
 <template>
+    <div class="row">
 
-    <div>
-        Resource
+        <div class="col-12">
+            <h2>Github: </h2>
+            <a class="small mt-1" href="https://github.com/farhad1372/flow-analytics" target="_blank">FLOW Analytics Github</a>
+
+            <h2 class="mt-8">Shroomdk</h2>
+            <a class="small mt-1" href="https://sdk.flipsidecrypto.xyz/shroomdk">Shroomedk</a>
+        </div>
     </div>
 </template>
 
